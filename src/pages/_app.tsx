e2a10306377a8a11f0983485629a619a53119662
Arthur Castilhos <arthurcastilhos@gmail.com>
2021-02-28 19:11:13 -0300

    fix: _app.tsx bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,7 @@
 import '../styles/global.css'
 
-import { ChallengesProvider } from '../contexts/ChallengeContext'
-
 function MyApp({ Component, pageProps }) {
-  return (
-  <ChallengesProvider>
-    <Component {...pageProps}/>
-  </ChallengesProvider>)}
+  return <Component {...pageProps}/>;
+}
 
 export default MyApp
